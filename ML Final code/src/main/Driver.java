9a1f56cc378d8546ef7bf25523d8456490618383
Derekmod <derekmod@gmail.com>
2015-12-09 03:47:14 -0500

    forgot to commit some files

--- ML Final code/src/main/Driver.java
+++ ML Final code/src/main/Driver.java
@@ -2,9 +2,10 @@ package main;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 public final class Driver {
-	public static final String DATA_FILE = "";
+	public static final String DATA_FILE = "C://Users/Derek/My Documents/Million Song Data/train_triplets.txt";
 	
 	/** Learning Params */
 	public static final int _f = 80;
@@ -12,17 +13,33 @@ public final class Driver {
 	public static final int _iterations = 100;
 	
 	/** Main Variables */
+	protected static int _M; //#users
+	protected static int _N; //#item
+	protected static ArrayList<String> _UserIDs;
+	protected static ArrayList<String> _ItemIDs;
 	protected static ArrayList<HashMap<Integer, Double>> _r;
 	protected static ArrayList<Double[]> _X;
 	protected static ArrayList<Double[]> _Y;
 	
 	public static void main(String[] args) {
 		
-		DataImporter.importData(_r, DATA_FILE);
+		DataImporter.importData(DATA_FILE);
+		
+		printData();
 		
 		//TODO: @Zach fill this out to match the signature you want
 		//Trainer.trainModel()
 		
 		//TODO: recommend
+	}
+	
+	private static void printData() {
+		for (int u=0; u<_M; u++) {
+			System.out.print("User ["+_UserIDs.get(u)+"]: ");
+			for (Entry<Integer, Double> entry : _r.get(u).entrySet()) {
+				System.out.print(""+entry.getKey() + ":"+entry.getValue() + " ");
+			}
+			System.out.println();
+		}
 	}
 }
