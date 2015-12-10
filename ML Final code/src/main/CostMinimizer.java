b3ba485983fd0375d763f8498a6e499d0da02c2c
Zach Sabin <zacharysabin@gmail.com>
2015-12-10 15:45:02 -0500

    Fixed for static structure

--- ML Final code/src/main/CostMinimizer.java
+++ ML Final code/src/main/CostMinimizer.java
@@ -3,87 +3,110 @@ package main;
 import java.util.stream.IntStream;
 
 import Jama.Matrix;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 
 public class CostMinimizer {
   
  
-  public static void minCost(int[][] r) {
-    Matrix y = new Matrix(5,5,5);
-    Matrix x = new Matrix(5,5,5);
-    
+  public static void minCost() {
+    Driver._Y = new Matrix(Driver._N,Driver._f,1);
+    Driver._X = new Matrix(Driver._M, Driver._f,1);
+    //N = # items
     boolean converged = false;
     
     double lambda = 0.5;
     
-    Matrix lambdaI1 = new Matrix(y.getColumnDimension(), y.getColumnDimension(), lambda);
-    Matrix lambdaT2 = new Matrix(x.getColumnDimension(), x.getColumnDimension(), lambda);    
-    Matrix Ii = new Matrix(r.length, r.length, 1);
-    Matrix Iu = new Matrix(r[0].length, r[0].length, 1);
+    Matrix lambdaI1 = new Matrix(Driver._Y.getColumnDimension(), Driver._Y.getColumnDimension(), lambda);
+    Matrix lambdaT2 = new Matrix(Driver._X.getColumnDimension(), Driver._X.getColumnDimension(), lambda);    
+    Matrix Ii = new Matrix(Driver._M, Driver._M, 1);
+    Matrix Iu = new Matrix(Driver._N, Driver._N, 1);
     
     while (!converged) {
         
       /*
        * Compute the Yus
        */
-      Matrix xT = x.transpose();
-      Matrix XX = xT.times(x);
+      Matrix xT = Driver._X.transpose();
+      Matrix XX = xT.times(Driver._X);
       
-      for (int i=0; i<y.getRowDimension(); i++) {
-        Matrix C = new Matrix(createcSquarei(r, i)).minus(Ii);
+      for (int i=0; i<Driver._Y.getRowDimension(); i++) {
+        Matrix C = new Matrix(createcSquarei(i)).minus(Ii);
         Matrix XtCI = xT.times(C).plus(lambdaT2);
         Matrix inverted = XX.plus(XtCI).inverse();
-        Matrix Yu = inverted.times(xT).times(C).times(new Matrix(computePi(r, i)));
-        y.setMatrix(i,i, 0, y.getColumnDimension(), Yu);
+        Matrix Yu = inverted.times(xT).times(C).times(new Matrix(computePi(i)));
+        Driver._Y.setMatrix(i,i, 0, Driver._Y.getColumnDimension(), Yu);
       }
       
       /*
        * Compute the Xis
        */
       
-      Matrix yT = y.transpose();
-      Matrix YY = yT.times(y);
+      Matrix yT = Driver._Y.transpose();
+      Matrix YY = yT.times(Driver._Y);
             
-      for (int u=0; u<x.getRowDimension(); u++) {
-        Matrix C = new Matrix(createcSquareu(r, u)).minus(Iu);
+      for (int u=0; u<Driver._X.getRowDimension(); u++) {
+        Matrix C = new Matrix(createcSquareu(u)).minus(Iu);
         Matrix YtCI = yT.times(C).plus(lambdaI1);
         Matrix inverted = YY.plus(YtCI).inverse();
-        Matrix Xu = inverted.times(yT).times(C).times(new Matrix(computePu(r, u)));
-        x.setMatrix(u,u, 0, y.getColumnDimension(), Xu);
+        Matrix Xu = inverted.times(yT).times(C).times(new Matrix(computePu(u)));
+        Driver._X.setMatrix(u,u, 0, Driver._Y.getColumnDimension(), Xu);
       }
     
     }
     
   }
   
-  private static double[][] computePi(int[][] r, int i) {
-    double[][] P = new double[1][r.length];
-    for (int u=0; u<r.length; u++) {
-      P[0][u] = (r[u][i] > 0) ? 0 : 1;
+  private static double[][] computePi(int i) {
+    double[][] P = new double[1][Driver._M];
+    HashMap<Integer, Double> item = Driver._rt.get(i);
+    for (int u=0; u<Driver._M; u++) {
+      if (item.containsKey(u)) {
+        P[0][i] = 1;
+      } else {
+        P[i][i] = 0; //-1?
+      }
     }
     return P;
   }
   
-  private static double[][] computePu(int[][] r, int u) {
-    double[][] P = new double[1][r[0].length];
-    for (int i=0; i<r.length; i++) {
-      P[0][i] = (r[u][i] > 0) ? 0 : 1;
+  private static double[][] computePu(int u) {
+    double[][] P = new double[1][Driver._N];
+    HashMap<Integer, Double> user = Driver._r.get(u);
+    for (int i=0; i<Driver._N; i++) {
+      if (user.containsKey(i)) {
+        P[0][i] = 1;
+      } else {
+        P[i][i] = 0; //-1?
+      }
     }
     return P;
   }
   
-  private static double[][] createcSquarei(int[][] r, int i) {
-    double[][] C = new double[r.length][r.length];
-    for (int u=0; u<r.length; u++) {
-      C[u][u] = computeConfidence(r[u][i]); //-1?
+  private static double[][] createcSquarei(int i) {
+    double[][] C = new double[Driver._M][Driver._M];
+    HashMap<Integer, Double> item = Driver._rt.get(i);
+    for (int u=0; u<Driver._N; u++) {
+      if (item.containsKey(u)) {
+        C[i][i] = computeConfidence(item.get(u));
+      } else {
+        C[i][i] = 1; //-1?
+      }
     }
     return C;
   }
   
-  private static double[][] createcSquareu(int[][] r, int u) {
-    double[][] C = new double[r[0].length][r[0].length];
-    for (int i=0; i<r[0].length; i++) {
-      C[i][i] = computeConfidence(r[u][i]); //-1?
+  private static double[][] createcSquareu(int u) {
+    double[][] C = new double[Driver._N][Driver._N];
+    HashMap<Integer, Double> user = Driver._r.get(u);
+    for (int i=0; i<Driver._N; i++) {
+      if (user.containsKey(i)) {
+        C[i][i] = computeConfidence(user.get(i));
+      } else {
+        C[i][i] = 1; //-1?
+      }
     }
     return C;
   }
@@ -110,7 +133,7 @@ public class CostMinimizer {
   return res;
   }
   
-  private static double computeConfidence(int rValue) {
+  private static double computeConfidence(double rValue) {
     return 1 + 10*rValue;
   }
   
