418190a2601d8765b63fd809724693926d31bc6c
Derekmod <derekmod@gmail.com>
2015-12-13 15:56:05 -0500

    ListSimilarity now works for things other than Objects

--- ML Final code/src/utilities/ListSimilarity.java
+++ ML Final code/src/utilities/ListSimilarity.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class ListSimilarity {
 
-	public double basicDifference(ArrayList<Object> a, ArrayList<Object> b) {
+	public static double basicDifference(ArrayList<? extends Object> a, ArrayList<? extends Object> b) {
 		double dif = 0.0;
 		
 		for (int i=0 ;i<a.size(); i++) {
